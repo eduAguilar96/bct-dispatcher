736de29519b826646aba2267593b9370bdb20948
eduAguilar96 <eduxx77@gmail.com>
2019-11-20 13:36:13 -0600

    Re-do initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+let express = require( "express" );
+let morgan = require( "morgan" );
+let mongoose = require( "mongoose" );
+let bodyParser = require( "body-parser" );
+// const { DATABASE_URL, PORT } = require( './config' );
+
+let app = express();
+let jsonParser = bodyParser.json();
+mongoose.Promise = global.Promise;
+
+app.use(express.static("public"));
+
+app.use(morgan( "dev" ));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+  console.log("get home");
+  res.sendFile('/public/server-list/index.html', {root: __dirname});
+});
+
+let server;
+
+function runServer(port, databaseUrl){
+	return new Promise( (resolve, reject ) => {
+		mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true}, response => {
+			if ( response ){
+				return reject(response);
+			}
+			else{
+				server = app.listen(port, () => {
+					console.log( "App is running on port " + port );
+					resolve();
+				})
+				.on( 'error', err => {
+					mongoose.disconnect();
+					return reject(err);
+				})
+			}
+		});
+	});
+}
+
+function closeServer(){
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close( err => {
+					if (err){
+						return reject(err);
+					}
+					else{
+						resolve();
+					}
+				});
+			});
+		});
+}
+
+runServer( "8080", "mongodb://localhost/lobbyServer" )
+	.catch( err => {
+		console.log( err );
+	});
+
+module.exports = { app, runServer, closeServer };
