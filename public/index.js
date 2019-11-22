0a25a7d83d0945c0f8b3f9037d9d0f8927e64d6e
eduAguilar96 <eduxx77@gmail.com>
2019-11-22 03:21:22 -0600

    Create comment container

--- public/index.js
+++ public/index.js
@@ -37,6 +37,9 @@ function lobbyLi(lobby){
         </svg>
       </button>
     </div>
+    <button class="btn btn-outline-secondary btn-lobby-details" type="button" data-toggle="collapse" data-target="#lobby-`+lobby.name+`" aria-expanded="false" aria-controls="lobby-`+lobby.name+`">
+      ...
+    </button>
     <span class="player-counter">
       <svg id="i-user" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="21" height="21" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
         <path d="M22 11 C22 16 19 20 16 20 13 20 10 16 10 11 10 6 12 3 16 3 20 3 22 6 22 11 Z M4 30 L28 30 C28 21 22 20 16 20 10 20 4 21 4 30 Z" />
@@ -47,6 +50,14 @@ function lobbyLi(lobby){
       `+((lobby.started) ? "- Game Started -" : "")+`
     </span>
     <span id="name-container">`+lobby.name+`</span>
+    <div id=lobby-`+lobby.name+` class="collapse">
+      <div class="lobby-details">
+        <ul class="list-group">
+          <li class="list-group list-group-item">Name: Comment</li>
+          <li class="list-group list-group-item">Name: Comment</li>
+        </ul>
+      </div>
+    </div>
   </li>
   `
   return li;
@@ -66,11 +77,11 @@ function update_list() {
   $.each(global_list, (index, lobby) => {
     if(fav_names.includes(lobby.name)){
       lobby.fav = true;
-      $('#fav-list-container ul').append(lobbyLi(lobby));
+      $('#fav-list-container .top-list').append(lobbyLi(lobby));
     }
     else{
       lobby.fav = false;
-      $('#lobby-list-container ul').append(lobbyLi(lobby));
+      $('#lobby-list-container .top-list').append(lobbyLi(lobby));
     }
   });
 }
