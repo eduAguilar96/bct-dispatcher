cce9079f8bc95f4b1d38592fd80c64b241ac8d4d
eduAguilar96 <eduxx77@gmail.com>
2019-11-22 00:29:02 -0600

    Set up mongo cluster

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/lobbiesDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://edag:<password>@cluster0-7qmxt.mongodb.net/test?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
